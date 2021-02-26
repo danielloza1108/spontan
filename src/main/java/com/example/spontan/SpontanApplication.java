a71da214108fbca7879b632bab18af59bfc746ec
Daniel <danielloza1108@gmail.com>
2021-02-26 19:09:13 +0100

    Initial commit

---
+++ src/main/java/com/example/spontan/SpontanApplication.java
@@ -0,0 +1,13 @@
+package com.example.spontan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpontanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpontanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spontan/SpontanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spontan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpontanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
