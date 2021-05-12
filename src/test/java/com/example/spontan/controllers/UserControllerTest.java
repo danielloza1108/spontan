03ce79355a1c982742d2a7d47bf150afba11494d
Rafał Paliwoda <r.paliwoda992@gmail.com>
2021-05-12 21:49:24 +0200

    add test to getUserByEmail endpoint.

--- src/test/java/com/example/spontan/controllers/UserControllerTest.java
+++ src/test/java/com/example/spontan/controllers/UserControllerTest.java
@@ -3,27 +3,28 @@ package com.example.spontan.controllers;
 import com.example.spontan.dto.EventDTO;
 import com.example.spontan.dto.UserDTO;
 import com.example.spontan.service.UserService;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = UserController.class)
 class UserControllerTest {
 
     private static final String ENDPOINT_ADD_USER = "/api/user/add";
     private static final String USER_NAME = "Rafal  ";
-    private static final String USER_EMAIL = "rafal@email.com" ;
+    private static final String USER_EMAIL = "rafal@email.com";
+    private static final String ENDPOINT_GET_USER_BY_EMAIL = "/api/user/get/{email}";
 
     @Autowired
     private MockMvc mockMvc;
@@ -56,7 +57,17 @@ class UserControllerTest {
     }
 
     @Test
-    void getUser() {
+    void shouldReturnUserFoundByEmail() throws Exception {
+
+        UserDTO userDTO = getUserDto();
+        String requestedUserDTO = objectMapper.writeValueAsString(userDTO);
+
+        when(userService.getUserByEmail(USER_EMAIL)).thenReturn(getUserDto());
+        mockMvc.perform(get(ENDPOINT_GET_USER_BY_EMAIL, USER_EMAIL))
+                .andExpect(status().isOk())
+                .andExpect(content().json(requestedUserDTO))
+                .andExpect(jsonPath("$.name").value(USER_NAME));
+
     }
 
     @Test
